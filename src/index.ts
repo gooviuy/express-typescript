ef0f57da94b22268f2dcba67229c6ddb8ca3db8b
gooviuy <gooviuy@outlook.com>
2022-05-17 15:58:24 -0300

    first setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json()); // middleware que transforla la req.body a un json
+
+const PORT = 3001;
+
+app.get("/ping", (req, res) => {
+  console.log("someone pinged here!");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log("Servver running");
+});
