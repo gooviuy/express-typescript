51bb551b94f8559bf382c9c962b4b0091d1e7358
gooviuy <gooviuy@outlook.com>
2022-05-18 11:43:07 -0300

    getting and posting new diary info

--- src/routes/diares.ts
+++ src/routes/diares.ts
@@ -7,8 +7,24 @@ router.get("/", (_req, res) => {
   res.send(diaryServices.getEntresWithoutSenstiveInfo());
 });
 
-router.post("/", (_req, res) => {
-  res.send("saving a diary!");
+router.get("/:id", (req, res) => {
+  // el + indica que lo que vamos a estar a estar recibiendo lo pasamos a tipo number
+  //ya que por defecto entra como string
+  const diary = diaryServices.findById(+req.params.id);
+
+  return diary != null ? res.send(diary) : res.sendStatus(404);
+});
+
+router.post("/", (req, res) => {
+  const { date, weather, visibilty, comment } = req.body;
+
+  const newDiaryEntry = diaryServices.addEntry(
+    date,
+    weather,
+    visibilty,
+    comment
+  );
+  res.json(newDiaryEntry);
 });
 
 export default router;
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,8 +1,22 @@
-import { DiaryEntry, NonSensitiveInfoEntry } from "../types";
+import {
+  DiaryEntry,
+  NonSensitiveInfoEntry,
+  Visibility,
+  Weather,
+} from "../types";
 import diaryData from "./diary.json";
 
 //utilizamos asercion de tipos
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+
+export const findById = (id: number): NonSensitiveInfoEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  if (entry != null) {
+    const { comment, ...restOfDiary } = entry;
+    return restOfDiary;
+  }
+  return undefined;
+};
 
 export const getEntries = (): DiaryEntry[] => diaries;
 
@@ -19,4 +33,20 @@ export const getEntresWithoutSenstiveInfo = (): NonSensitiveInfoEntry[] => {
   });
 };
 
-export const addEntry = (): undefined => undefined;
+export const addEntry = (
+  date: string,
+  weather: Weather,
+  visibilty: Visibility,
+  comment: string
+): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    date,
+    weather,
+    visibilty,
+    comment,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
